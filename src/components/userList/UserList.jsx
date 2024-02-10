a1b627e58a5444c020ab6a8881adcf26f9cca782
Mamun-Ur-Rashid <engmamun656@gmail.com>
2024-02-10 10:27:12 +0600

    search function implement

--- src/components/userList/UserList.jsx
+++ src/components/userList/UserList.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
+    const [searchText, setSearchText ] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,24 +21,37 @@ const UserList = () => {
         fetchData();
     }, [])
 
+    // Search option
+    const filterUsers = users.filter((user) => `${user.firstName} ${user.lastName}`.toLowerCase().includes(searchText.toLowerCase()));
+
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-2 md:p-5 md:mt-20'>
-            
-            {
-                users.map((user) =>
-                    <div key={user.id}>
-                        <div className="card w-full h-[450px]  shadow-sm rounded-lg bg-[#a8b2bd83]">
-                            <figure><img className='h-[200px] pt-6 md:pt-4' src={user.image} alt="Shoes" /></figure>
-                            <div className="card-body text-xl md:text-sm">
-                                <Link to={`/userDetails/${user.id}`}><h2 className="card-title hover:text-2xl" > {user.firstName}</h2></Link>
-                                <h4>Last Name: {user.lastName}</h4>
-                                <p>Email: {user.email}</p>
-                                <p>Company Name: {user.company.name}</p>
-                                <p className=''>Adrress: {user.address.address}, {user.address.city}, {user.address.state}</p>
+        <div className='px-20 mt-10'>
+            <div>
+                <div>
+                    <input type='text' placeholder='Search by name...' value={searchText} onChange={(e) =>setSearchText(e.target.value)} className='md:ml-16 p-1 px-2 border rounded-lg'></input>
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-2 md:p-5 md:mt-8'>
+
+                {
+                    filterUsers.map((user) =>
+                        <div key={user.id}>
+                            <div className="card w-96 h-[350px]  shadow-sm rounded-lg bg-[#a8b2bd83]">
+                                <figure><img className='h-[140px] bg-slate-200 p-2 mt-4 md:pt-4 rounded-full' src={user.image} alt="Shoes" /></figure>
+                                <div className="card-body text-xl md:text-sm">
+                                    <Link to={`/userDetails/${user.id}`}><h2 className="card-title hover:text-2xl" > {user.firstName}</h2></Link>
+                                    <h4>Last Name: {user.lastName}</h4>
+                                    <p>Email: {user.email}</p>
+                                    <p>Company Name: {user.company.name}</p>
+                                    <p className=''>Adrress: {user.address.address}, {user.address.city}, {user.address.state}</p>
+                                </div>
                             </div>
-                        </div>
-                    </div>)
-            }
+                        </div>)
+                }
+            </div>
         </div>
     );
 };
